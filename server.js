6235a3dd27fef6e050a9e926d43b376e11b25fff
Azamat <azamatomonkeldiyev@gmail.com>
2025-03-18 17:02:30 +0500

    file src create

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require("express");
+require("dotenv").config({path: "./src/config/.env"});
 
+const express = require("express");
 const app = express();
 
-app.listen(3000, () => console.log("server started on 3000 port"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
