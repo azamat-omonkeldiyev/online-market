e614827878dc73a2fee7e586c09dfce6b679a9a7
Azamat <azamatomonkeldiyev@gmail.com>
2025-03-19 12:05:36 +0500

    role middleware added

---
+++ src/rolemiddleware/roleAuth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const roleMiddleware = (roles) => {
+    return (req,res,next) => {
+        let token = req.header("Authorization")?.split(" ")[1]; 
+        console.log(token);
+        if (!token) {
+            return res.status(400).send({ message: "Access denied. No token provided." });
+        }
+    
+        try {
+            let data = jwt.verify(token, process.env.JWT_SECRET);
+
+            if(roles.includes(data.role)){
+                req.userId = data.id;
+                req.userRole = data.role 
+                next();
+            }else{
+                res.status(401).send("Not allowed");
+            }
+        } catch (error) {
+            res.status(401).send({ message: "Invalid token" });
+        }
+
+    }
+};
+
+module.exports = roleMiddleware;
